ed5ed35b3d3f849eea1d7c79f090cbfa830b80e1
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2025-02-21 13:01:58 +0500

    Apartments logic

--- backend/src/global-router.ts
+++ backend/src/global-router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import authRouter from './auth/auth-router';
 import userRouter from './profile/user-router';
+import apartmentRouter from './apartment/apartment.router';
 
 const globalRouter = Router();
 
 globalRouter.use(authRouter);
 globalRouter.use('/users', userRouter)
+globalRouter.use('/apartments', apartmentRouter);
+
 
 export default globalRouter;
